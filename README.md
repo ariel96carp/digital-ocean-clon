9ac935ccb9f2e1dfe3c6940fb8c6c408262cb421
Dante Carrasco <arielcarp1996@gmail.com>
2023-02-06 14:21:37 -0300

    adding new assets and creating a readme file

---
+++ README.md
@@ -0,0 +1,34 @@
+# DigitalOcean UI Clon
+DigitalOcean's Main Page UI Clon
+
+## Installation
+1. Clone a copy of the repository to your local machine.
+```bash
+git clone git@github.com:ariel96carp/ecommerce-dante-app.git
+```
+
+2. Use the package npm to install dependencies.
+```bash
+npm i
+```
+
+3. Run this repository on local environment.
+```bash
+npm run dev
+```
+
+4. *[optional]* You can build a dist with the command:
+```bash
+npm run production
+```
+
+## Overview
+![Overview](./img/localhost_3000_%20(3).png)
+![Overview](./img/localhost_3000_%20(4).png)
+![Overview](./img/localhost_3000_%20(6).png)
+![Overview](./img/localhost_3000_%20(7).png)
+![Overview](./img/localhost_3000_.png)
+![Overview](./img/localhost_3000_(Samsung%20Galaxy%20S8%2B).png)
+
+## Deploy
+[https://digitalocean-clon.netlify.app](https://digitalocean-clon.netlify.app)
--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-window.addEventListener("DOMContentLoaded", () => {
+window.addEventListener('DOMContentLoaded', () => {
     const setHeaderSize = () => {
         const headerSection = document.getElementById("page-header")
         const headerHeight = parseFloat(getComputedStyle(headerSection).height)
@@ -8,161 +8,149 @@ window.addEventListener("DOMContentLoaded", () => {
             document.documentElement.style.setProperty("--header-size", `${headerHeight}px`)
         }
     }
-
-    window.addEventListener("load", setHeaderSize)
-    window.addEventListener("resize", setHeaderSize)
-
-    const toggleBodyOverflow = (areOpenSubmenus = false) => {
-        if (areOpenSubmenus === false){
+    const toggleBodyOverflow = (isAnySubmenuOpened = false) => {
+        if (isAnySubmenuOpened === false){
             const bodyOverflow = document.body.style.overflow
             switch(bodyOverflow){
-                case "":
-                    document.body.style.overflow="hidden"
+                case '':
+                    document.body.style.overflow='hidden'
                     break;
-                case "visible":
-                    document.body.style.overflow="hidden"
+                case 'visible':
+                    document.body.style.overflow='hidden'
                     break;
                 default:
-                    document.body.style.overflow="visible"
+                    document.body.style.overflow='visible'
             }
         }
     }
-
-    const hideSubmenuOnOpen = (indexGiven) => {
-        return new Promise(( resolve, reject ) => {
-            let areOpenSubmenus = false
-            const mainMenu = document.getElementById("menu")
-            const menuItems = mainMenu.querySelectorAll(".item")
-            for (let i = 0; i < menuItems.length; i++){
-                const submenu = menuItems[i].querySelector(".submenu")
-                if (submenu){
-                    if (i !== indexGiven){
-                        if (submenu.classList.contains("show")){
-                            submenu.scrollTo(0, 0)
-                            const itemLink = submenu.previousElementSibling
-                            submenu.classList.toggle("show")
-                            itemLink.classList.toggle("open")
-                            areOpenSubmenus = true
-                            break
-                        }
+    const hideSubmenuOnOpen = (givenIndex) => new Promise((resolve) => {
+        let isAnySubmenuOpened = false
+        const mainMenu = document.getElementById('menu')
+        const menuItems = mainMenu.querySelectorAll('.item')
+        for (let i = 0; i < menuItems.length; i++){
+            const submenu = menuItems[i].querySelector('.submenu')
+            if (submenu){
+                if (i !== givenIndex){
+                    if (submenu.classList.contains('show')){
+                        submenu.scrollTo(0, 0)
+                        const itemLink = submenu.previousElementSibling
+                        submenu.classList.toggle('show')
+                        itemLink.classList.toggle('open')
+                        isAnySubmenuOpened = true
+                        break
                     }
                 }
             }
-            resolve(areOpenSubmenus)
-        })
-    }
-
+        }
+        resolve(isAnySubmenuOpened)
+    })
     const showSubmenu = () => {
-        const mainMenu = document.getElementById("menu")
-        const menuItems = mainMenu.querySelectorAll(".item")
+        const mainMenu = document.getElementById('menu')
+        const menuItems = mainMenu.querySelectorAll('.item')
         menuItems.forEach((item, index) => {
-            item.addEventListener("click", async () => {
-                const submenu = item.querySelector(".submenu")
+            item.addEventListener('click', async () => {
+                const submenu = item.querySelector('.submenu')
                 if (submenu){
                     const areOpenSubmenus = await hideSubmenuOnOpen(index)
                     const itemLink = submenu.previousElementSibling
                     submenu.scrollTo(0, 0)
-                    itemLink.classList.toggle("open")
-                    submenu.classList.toggle("show")
+                    itemLink.classList.toggle('open')
+                    submenu.classList.toggle('show')
                     toggleBodyOverflow(areOpenSubmenus)
                 }
             })
         })
     }
-    showSubmenu()
-
     const hideSubmenuOnResize = () => {
-        const largeBp = matchMedia("(max-width: 1024px)")
-        if (largeBp.matches){
-            const mainMenu = document.getElementById("menu")
-            const menuItems = mainMenu.querySelectorAll(".item")
+        const LARGE_BP = matchMedia('(max-width: 1024px)')
+        if (LARGE_BP.matches){
+            const mainMenu = document.getElementById('menu')
+            const menuItems = mainMenu.querySelectorAll('.item')
             for (let item of menuItems){
-                const submenu = item.querySelector(".submenu")
+                const submenu = item.querySelector('.submenu')
                 if (submenu){
                     const itemLink = submenu.previousElementSibling
-                    if (submenu.classList.contains("show")){
-                        itemLink.classList.toggle("open")
-                        submenu.classList.toggle("show")
+                    if (submenu.classList.contains('show')){
+                        itemLink.classList.toggle('open')
+                        submenu.classList.toggle('show')
                         toggleBodyOverflow()
                     }
                 }
             }
         }
     }
-    window.addEventListener("resize", hideSubmenuOnResize)
-
     const showOffCanvas = () => {
-        const offCanvasButton = document.getElementById("offcanvas-button")
-        offCanvasButton.addEventListener("click", () => {
-            const offCanvas = document.getElementById("offcanvas")
-            offCanvas.classList.toggle("show")
+        const offCanvasButton = document.getElementById('offcanvas-button')
+        offCanvasButton.addEventListener('click', () => {
+            const offCanvas = document.getElementById('offcanvas')
+            offCanvas.classList.toggle('show')
             toggleBodyOverflow()
         })
     }
-    showOffCanvas()
-
     const hideOffCanvasOnResize = () => {
-        const largeBp = matchMedia("(min-width: 1024px)")
-        if (largeBp.matches){
-            const offCanvas = document.getElementById("offcanvas")
-            if (offCanvas.classList.contains("show")){
-                offCanvas.classList.toggle("show")
+        const LARGE_BP = matchMedia('(min-width: 1024px)')
+        if (LARGE_BP.matches){
+            const offCanvas = document.getElementById('offcanvas')
+            if (offCanvas.classList.contains('show')){
+                offCanvas.classList.toggle('show')
                 toggleBodyOverflow()
             }
         }
     }
-    window.addEventListener("resize", hideOffCanvasOnResize)
-
     const hideMenuOnLogoClick = () => {
-        const logo = document.getElementById("main-logo")
-        logo.addEventListener("click", () => {
-            const largeBp = matchMedia("(min-width: 1024px")
-            if (largeBp.matches){
-                const desktopMenu = document.getElementById("menu")
-                const desktopItems = desktopMenu.querySelectorAll(".item")
+        const logo = document.getElementById('main-logo')
+        logo.addEventListener('click', () => {
+            const LARGE_BP = matchMedia('(min-width: 1024px')
+            if (LARGE_BP.matches){
+                const desktopMenu = document.getElementById('menu')
+                const desktopItems = desktopMenu.querySelectorAll('.item')
                 for (let item of desktopItems){
-                    const submenu = item.querySelector(".submenu")
+                    const submenu = item.querySelector('.submenu')
                     if (submenu){
                         const itemLink = submenu.previousElementSibling
-                        if (submenu.classList.contains("show")){
-                            itemLink.classList.toggle("open")
-                            submenu.classList.toggle("show")
+                        if (submenu.classList.contains('show')){
+                            itemLink.classList.toggle('open')
+                            submenu.classList.toggle('show')
                             toggleBodyOverflow()
                         }
                     }
                 }
             }
             else {
-                const offCanvas = document.getElementById("offcanvas")
-                if (offCanvas.classList.contains("show")){
-                    offCanvas.classList.toggle("show")
+                const offCanvas = document.getElementById('offcanvas')
+                if (offCanvas.classList.contains('show')){
+                    offCanvas.classList.toggle('show')
                     toggleBodyOverflow()
                 }
             }
         })
     }
-    hideMenuOnLogoClick()
-
     const setCarouselMaxWidth = () => {
-        const brandSection = document.getElementById("brands")
-        const smallBp = matchMedia("(max-width: 640px)")
+        const brandSection = document.getElementById('brands')
+        const smallBp = matchMedia('(max-width: 640px)')
         if (smallBp.matches){
             const screenWidth = document.documentElement.getBoundingClientRect().width
-            brandSection.style.setProperty("--carousel-max-w", `${screenWidth}px`)
+            brandSection.style.setProperty('--carousel-max-w', `${screenWidth}px`)
         }
     }
-    setCarouselMaxWidth()
-    window.addEventListener("resize", setCarouselMaxWidth)
-
     const hideMenuOnItemClick = () => {
-        const offCanvasMenu = document.getElementById("offcanvas")
-        const offCanvasItems = offCanvasMenu.querySelectorAll(".offcanvas-item")
+        const offCanvasMenu = document.getElementById('offcanvas')
+        const offCanvasItems = offCanvasMenu.querySelectorAll('.offcanvas-item')
         for (let item of offCanvasItems){
-            item.addEventListener("click", () => {
-                offCanvasMenu.classList.toggle("show")
+            item.addEventListener('click', () => {
+                offCanvasMenu.classList.toggle('show')
                 toggleBodyOverflow()
             })
         }
     }
+    showSubmenu()
+    showOffCanvas()
+    hideMenuOnLogoClick()
+    setCarouselMaxWidth()
     hideMenuOnItemClick()
+    window.addEventListener('load', setHeaderSize)
+    window.addEventListener('resize', setHeaderSize)
+    window.addEventListener('resize', hideSubmenuOnResize)
+    window.addEventListener('resize', hideOffCanvasOnResize)
+    window.addEventListener('resize', setCarouselMaxWidth)
 })
